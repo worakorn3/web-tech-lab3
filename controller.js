23d7f19de8a1b845947edbfef736566b3b333478
worakorn3 <worakorn3@live.com>
2018-02-05 21:16:34 +0700

    Initial commit

---
+++ controller.js
@@ -0,0 +1,34 @@
+let quizCount = 0;
+
+// Utility function for ceiling questions
+function quiz10(){
+    
+    quizCount = 10;
+    console.log(quizCount);
+
+}
+
+function quiz20(){
+    
+    quizCount = 20;
+    console.log(quizCount);
+    
+}
+
+//
+
+function checkAns(answer) {
+    /*image change: 
+    default and wrong answer
+        https://media.giphy.com/media/xTiTnkFneD1je4rPOM/giphy.gif
+    correct answer
+        https://media.giphy.com/media/26ufdipQqU2lhNA4g/giphy.gif
+    
+    */
+    let b = document.getElementById("imgg")
+    if(answer){
+
+    } else {
+        $(img.resizer)
+    }
+}
